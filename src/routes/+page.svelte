ef9fd1f374b3a1d02c8476267dcf88bbc13236f5
Michael Aliendro <michael.aliendro@dentsu.com>
2023-07-12 17:34:51 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Counter from '$lib/components/Counter.svelte';
+	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+	import IconButton from '@smui/icon-button';
+</script>
+
+<main>
+	<TopAppBar variant="static" color="secondary" prominent={false} dense={false}>
+		<Row>
+			<Section>
+				<IconButton class="material-icons">menu</IconButton>
+				<Title>Field Management</Title>
+			</Section>
+			<Section align="end" toolbar>
+				<IconButton class="material-icons">circle</IconButton>
+				<IconButton class="material-icons">login</IconButton>
+			</Section>
+		</Row>
+	</TopAppBar>
+	<Counter />
+</main>
+
+<style lang="scss">
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
